0ec1b4bd2c341eb08cd3ad1a276ae2903fee13d5
Mark Smithson <mark@smithson.me>
2019-03-09 17:04:02 +0000

    basic rules

---
+++ main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "markers" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id = "${aws_vpc.markers.id}"
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "application" {
+  name = "Application"
+  description = "Security Group for the Application"
+  vpc_id = "${aws_vpc.markers.id}"
+}
+
+resource "aws_security_group_rule" "application_https" {
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.application.id}"
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "HTTPS from anywhere"
+}
+
+resource "aws_security_group" "logstash" {
+  name = "Logstash"
+  description = "Security Group for the Logstash Cluster"
+  vpc_id = "${aws_vpc.markers.id}"
+}
+
+resource "aws_security_group_rule" "logstash_5000" {
+  type = "ingress"
+  from_port = 500
+  to_port = 5000
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.logstash.id}"
+  source_security_group_id = "${aws_security_group.application.id}"
+  description = "Access to 5000 from application"
+}
+
+resource "aws_security_group" "elasticsearch" {
+  name = "Elasticsearch"
+  description = "Security Group for the Elasticsearch Cluster"
+  vpc_id = "${aws_vpc.markers.id}"
+}
+
+resource "aws_security_group_rule" "elastic_9200" {
+  type = "ingress"
+  from_port = 9200
+  to_port = 9200
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.elasticsearch.id}"
+  source_security_group_id = "${aws_security_group.logstash.id}"
+  description = "Access to 9200 from Logstash"
+}
