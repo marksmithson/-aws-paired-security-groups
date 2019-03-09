c6b5583cbfdfb09a080d83f6f9c4b42a2631a4fd
Mark Smithson <mark@smithson.me>
2019-03-09 17:13:58 +0000

    ingress rules from security groups

--- services.tf
+++ services.tf
@@ -21,3 +21,42 @@ resource "aws_security_group" "favourites_service" {
   description = "Security Group for the Favourites Service"
   vpc_id = "${aws_vpc.markers.id}"
 }
+
+resource "aws_security_group_rule" "logstash_application_service" {
+  type = "ingress"
+  from_port = 500
+  to_port = 5000
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.logstash.id}"
+  source_security_group_id = "${aws_security_group.application_service.id}"
+  description = "Access to 5000 from application service"
+}
+
+resource "aws_security_group_rule" "logstash_user_service" {
+  type = "ingress"
+  from_port = 500
+  to_port = 5000
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.logstash.id}"
+  source_security_group_id = "${aws_security_group.user_service.id}"
+  description = "Access to 5000 from user service"
+}
+resource "aws_security_group_rule" "logstash_search_service" {
+  type = "ingress"
+  from_port = 500
+  to_port = 5000
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.logstash.id}"
+  source_security_group_id = "${aws_security_group.search_service.id}"
+  description = "Access to 5000 from search service"
+}
+
+resource "aws_security_group_rule" "logstash_favourites_service" {
+  type = "ingress"
+  from_port = 500
+  to_port = 5000
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.logstash.id}"
+  source_security_group_id = "${aws_security_group.favourites_service.id}"
+  description = "Access to 5000 from favourites service"
+}
